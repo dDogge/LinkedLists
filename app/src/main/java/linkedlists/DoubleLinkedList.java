0c4beb6416c90b1dfb113ffdb6e275d1150316b6
dDogge <douglasfjallrud@gmail.com>
2024-10-08 22:56:27 +0200

    Inital commit

---
+++ app/src/main/java/linkedlists/DoubleLinkedList.java
@@ -0,0 +1,36 @@
+package linkedlists;
+
+public class DoubleLinkedList<E> {
+    
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E element, Node<E> next, Node<E> prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+    }
+}
---
+++ app/src/main/java/linkedlists/SingleLinkedList2.java
@@ -0,0 +1,26 @@
+package linkedlists;
+
+public class SingleLinkedList2<E> {
+    
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+}
