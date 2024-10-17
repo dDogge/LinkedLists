2a072d73cd7b9934fbd6bb5aa40c1d4845e94ad0
dDogge <douglasfjallrud@gmail.com>
2024-10-17 14:52:12 +0200

    More additions to the GUI

--- app/src/main/java/linkedlists/Vislink.java
+++ app/src/main/java/linkedlists/Vislink.java
@@ -9,7 +9,9 @@ public class Vislink {
     private JPanel optionBar;
 
     private JLabel setup;
+    private JLabel listToBeChosen;
     private JComboBox<String> listType;
+    private JLabel dataToBeChosen;
     private JComboBox<String> dataType;
 
     private JLabel addition;
@@ -18,6 +20,7 @@ public class Vislink {
     private JButton addFirst;
     private JButton addLast;
     private JButton add;
+    private JLabel indexToBeAdded;
     private JTextField addIndex;
 
     private JLabel removal;
@@ -52,6 +55,21 @@ public class Vislink {
         this.data = new JLabel("DATA");
         this.size = new JLabel("SIZE:");
         this.status = new JPanel();
+        this.listToBeChosen = new JLabel("CHOSE LIST TYPE");
+        this.dataToBeChosen = new JLabel("CHOSE DATA TYPE");
+
+        String[] listTypes = {
+            "Single Linked List",
+            "Cirkular Linked List",
+            "Double Linked List"
+        };
+        this.listType = new JComboBox<>(listTypes);
+
+        String[] dataTypes = {
+            "Integer", "Double",
+            "Char", "String" 
+        };
+        this.dataType = new JComboBox<>(dataTypes);
 
         data.setBounds(1510, 5, 40, 30);
         data.setForeground(Color.GREEN);
@@ -59,6 +77,27 @@ public class Vislink {
         size.setBounds(1800, 5, 50, 30);
         size.setForeground(Color.GREEN);
         size.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        setup.setBounds(10, 5, 60, 30);
+        setup.setForeground(Color.WHITE);
+        setup.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        addition.setBounds(350, 5, 80, 30);
+        addition.setForeground(Color.GREEN);
+        addition.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        removal.setBounds(700, 5, 80, 30);
+        removal.setForeground(Color.RED);
+        removal.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        uitility.setBounds(1050, 5, 80, 30);
+        uitility.setForeground(Color.MAGENTA);
+        uitility.setFont(new Font("Monospaced", Font.PLAIN, 16));
+
+        listToBeChosen.setBounds(10, 40, 150, 30);
+        listToBeChosen.setForeground(Color.WHITE);
+        listToBeChosen.setFont(new Font("Monospaced", Font.PLAIN, 16));        
+        listType.setBounds(10, 75, 150, 30);
+        dataToBeChosen.setBounds(10, 110, 150, 30);
+        dataToBeChosen.setForeground(Color.WHITE);
+        dataToBeChosen.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        dataType.setBounds(10, 145, 150, 30);
 
         field.setBackground(Color.BLACK);
         field.setBounds(0, 0, 1920, 760);
@@ -68,13 +107,21 @@ public class Vislink {
         optionBar.setBounds(0, 760, 1920, 200);
         optionBar.setLayout(null);
 
-        status.setBackground(Color.BLACK);  // Black background
-        status.setBounds(1510, 40, 400, 150);  // Placed on the right side of optionBar
+        status.setBackground(Color.BLACK);  
+        status.setBounds(1510, 40, 400, 150); 
         status.setLayout(null);
 
         optionBar.add(status);
         optionBar.add(data);
         optionBar.add(size);
+        optionBar.add(setup);
+        optionBar.add(addition);
+        optionBar.add(removal);
+        optionBar.add(uitility);
+        optionBar.add(listToBeChosen);
+        optionBar.add(listType);
+        optionBar.add(dataToBeChosen);
+        optionBar.add(dataType);
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.add(field);
